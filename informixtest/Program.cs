9a940ecb032c62517dc9e50d3208687564bcec53
Yamil <yamil.israel.ortega@pemex.com>
2019-09-05 16:58:39 -0500

    agregado

---
+++ informixtest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace informixtest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! mundial");
+        }
+    }
+}
