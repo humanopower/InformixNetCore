86f268226e422f76d914598616c8a88affa987fa
yamil Ortega Source tree <yamil.israel.ortega@pemex.com>
2019-09-05 18:40:24 -0500

    Se agrega linea para adicionar bash command

--- ConsolaInforTest/Program.cs
+++ ConsolaInforTest/Program.cs
@@ -1,5 +1,6 @@
 ﻿using IBM.Data.DB2.Core;
 using System;
+using System.Diagnostics;
 
 namespace ConsolaInforTest
 {
@@ -8,6 +9,9 @@ namespace ConsolaInforTest
 		static void Main(string[] args)
 		{
 			Console.WriteLine("Hello World!");
+			// lets say we want to run this command:    
+			//  t=$(echo 'this is a test'); echo "$t" | grep -o 'is a'
+			var output = ExecuteBashCommand("t=$(echo 'this is a test'); echo \"$t\" | grep -o 'is a'");
 
 
 			try
@@ -44,6 +48,30 @@ namespace ConsolaInforTest
 				Console.WriteLine($"Counter: {counter}");
 				System.Threading.Tasks.Task.Delay(1000).Wait();
 			}
+		}
+
+		static string ExecuteBashCommand(string command)
+		{
+			// according to: https://stackoverflow.com/a/15262019/637142
+			// thans to this we will pass everything as one command
+			command = command.Replace("\"", "\"\"");
+
+			var proc = new Process
+			{
+				StartInfo = new ProcessStartInfo
+				{
+					FileName = "/bin/bash",
+					Arguments = "-c \"" + command + "\"",
+					UseShellExecute = false,
+					RedirectStandardOutput = true,
+					CreateNoWindow = true
+				}
+			};
+
+			proc.Start();
+			proc.WaitForExit();
+
+			return proc.StandardOutput.ReadToEnd();
 		}
 	}
 }
